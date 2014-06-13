86d5bfa0445396beac47f6c6e09ca9d63b7bb9f8
Scott Neel <scott.neel@am.sony.com>
2014-06-12 19:23:42 -0700

    * add stylesheets

--- src/propulsion-grid.js
+++ src/propulsion-grid.js
@@ -15,20 +15,20 @@ define('propulsion-grid', ['angular', 'angular-grid', 'angular-ui-bootstrap', 'a
       scope: {
         config: "=config"
       },
-      template: '<div></div>',
+      template: '<div class="propulsion-grid"></div>',
       link: function (scope, elem, attrs) {
         var flexHeight = new window.ngGridFlexibleHeightPlugin();
 
-        scope.data = [{name: 'test'}, {name: 'test2'}];
+        scope.data = [{name: 'test'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}, {name: 'test2'}];
         scope.records_start = 1;
         scope.records_end = 2;
         scope.records_total = 2;
         scope.records_page = 1;
         scope.records_limit = 10;
+        scope.pageSizes = [10, 25, 50, 100, 250, 500];
 
         scope.gridConfig = {
           data: 'data',
-          plugins: [flexHeight],
           enableRowSelection: false,
           rowTemplate: '<div ng-click="$state.go(\'^.record.view\', row.entity)" style="cursor: pointer" ng-style="{ \'pointer\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell {{col.cellClass}}"><div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }">&nbsp;</div><div ng-cell></div></div>',
           columnDefs: [
